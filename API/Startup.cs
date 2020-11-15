adcc86db25f8d269c09d48e88eadf3e0c920e345
sijopanadan <sijo_pthomas@yahoo.com>
2020-11-15 14:13:16 +0530

    angular project adding

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,7 @@ namespace API
             services.AddDbContext<DataContext>(options =>
                 options.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -50,7 +51,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
